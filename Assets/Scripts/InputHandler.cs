6646e4d45fcbc6aed58422f18e87af963e5f2591
tudta <jak3matt@gmail.com>
2017-06-11 22:51:50 -0700

    Command Pattern Start

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour {
+
+    private List<Command> commands = new List<Command>();
+    private List<KeyCode> keys = new List<KeyCode>();
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
